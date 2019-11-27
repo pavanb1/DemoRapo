52e552cc5ea361e67e48bd3a8ed16e2c51557574
pavan <pavan@DESKTOP-9CGM9OG>
2019-11-27 19:00:56 +0530

    Lounch Site sucessfuly

--- DemoRepo/src/javaPro/Demo.java
+++ DemoRepo/src/javaPro/Demo.java
@@ -1,9 +1,17 @@
 package javaPro;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "E:\\Automation Testing\\SeleniumWebDriverThree\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.amazon.in");
+		
 
 	}
 
