ae721edbee1ae3a37c233b42e304d4343ee29a8c
pavan <pavan@DESKTOP-9CGM9OG>
2019-12-12 14:40:16 +0530

    Add cmand line

--- DemoRepo/src/javaPro/Demo.java
+++ DemoRepo/src/javaPro/Demo.java
@@ -9,6 +9,7 @@ public class Demo {
 		System.setProperty("webdriver.chrome.driver", "E:\\Automation Testing\\SeleniumWebDriverThree\\Driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		//Login amazon
 		
 		driver.get("https://www.amazon.in");
 		
